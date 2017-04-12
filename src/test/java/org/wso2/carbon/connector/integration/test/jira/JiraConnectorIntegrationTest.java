11d9cc03a8b17f64421cfb988a1ec6d5b49626b8
Keerthika Mahendralingam <keerthu1211@GMAIL.COM>
2017-04-12 23:55:21 +0530

    Update JiraConnectorIntegrationTest.java

--- src/test/java/org/wso2/carbon/connector/integration/test/jira/JiraConnectorIntegrationTest.java
+++ src/test/java/org/wso2/carbon/connector/integration/test/jira/JiraConnectorIntegrationTest.java
@@ -45,7 +45,7 @@ public class JiraConnectorIntegrationTest extends ConnectorIntegrationTestBase {
     @BeforeClass(alwaysRun = true)
     public void setEnvironment() throws Exception {
 
-        init("jira-connector-1.0.3");
+        init("jira-connector-1.0.4-SNAPSHOT");
 
         esbRequestHeadersMap.put("Content-Type", "application/json");
 
